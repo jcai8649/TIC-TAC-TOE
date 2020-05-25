3b0ae30774b11a8a0c2e209078c75311aa76d613
Jackson Cai <jcai8649@gmail.com>
2020-05-25 16:01:22 -0700

    Add html structure

---
+++ app.js
@@ -0,0 +1,16 @@
+const Gameboard = (() => {
+    const board = [];
+})();
+
+const playerFactory = (name, symbol) =>{   
+
+    return {name,symbol}
+}
+
+function render(){
+
+}
+
+function addMark(){
+    
+}
