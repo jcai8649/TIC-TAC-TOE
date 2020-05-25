c518dd3eff49afb975048855aa6fcfdeb1a619a7
Jackson Cai <jcai8649@gmail.com>
2020-05-25 16:57:27 -0700

    Add render function

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 const Gameboard = (() => {
-    const board = [];
+    const gameBoard = ['X','O','O','X','X','O','X','O','X'];
+    return {gameBoard};
+})();
+
+const displayController = (() =>{
+    const render = (board) => {
+        for (let i = 0; i < board.length; i++){
+           let squareID = document.getElementById(`${i+1}`);
+           squareID.innerText = board[i];
+        }
+    }
+    return {render};
 })();
 
 const playerFactory = (name, symbol) =>{   
-
     return {name,symbol}
 }
 
-function render(){
-
-}
+displayController.render(Gameboard.gameBoard);
 
 function addMark(){
-    
+
 }
