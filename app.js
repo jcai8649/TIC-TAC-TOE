71999ee8b454626c657a5f3981ab046590e2e29c
Jackson Cai <jcai8649@gmail.com>
2020-05-25 22:30:24 -0700

    Add winCheck function

--- app.js
+++ app.js
@@ -6,23 +6,49 @@ const Gameboard = (() => {
 })();
 
 const displayController = (() =>{
+    
     const addMark = (id) =>{
         if(Gameboard.gameBoard[id-1] === ""){
             if(player1.theirTurn){
                 turn.innerText = "Player O's turn"
                 Gameboard.gameBoard[id-1] = player1.symbol;
                 render(Gameboard.gameBoard);
+                checkWin(player1.symbol);
                 player1.theirTurn = false;
                 player2.theirTurn = true;
             } else if (player2.theirTurn){
- 
                 turn.innerText = "Player X's turn"
                 Gameboard.gameBoard[id-1] = player2.symbol;
                 render(Gameboard.gameBoard);
+                checkWin(player2.symbol);
                 player1.theirTurn = true;
                 player2.theirTurn = false;
             }
         }
+    }
+
+    const checkWin = (symbol) => {
+
+        let win = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
+        let newArr = [];
+        for (let i = 0; i < Gameboard.gameBoard.length; i++){
+            if (symbol == Gameboard.gameBoard[i]) {
+                newArr.push(i+1);
+            }
+        }
+    
+        let checker = (arr, target) => target.every(v => arr.includes(v));
+
+        for (let i = 0; i < win.length; i++){
+            if (checker(newArr, win[i])){
+                turn.innerText = `${symbol} wins!` ;
+                break ;
+            }
+            else if (!Gameboard.gameBoard.includes("") && i === win.length - 1){
+                turn.innerText = "It's a tie!"
+                break;
+            }
+        } 
     }
 
     const render = (board) => {
@@ -31,16 +57,14 @@ const displayController = (() =>{
            squareID.innerText = board[i];
         }
     }
-
     return {addMark, render};
 })();
 
 const PlayerFactory = (name, symbol, theirTurn) =>{  
-    return {name,symbol, theirTurn}
+    return {name, symbol, theirTurn}
 }
 
 const player1 = PlayerFactory("player1", 'X', true);
 const player2 = PlayerFactory("player2", 'O', false);
-
 
 
