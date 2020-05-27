373432197cd1a62bf804ab7c65d659ddb18f09af
Jackson Cai <jcai8649@gmail.com>
2020-05-26 17:48:01 -0700

    Add restart function

--- app.js
+++ app.js
@@ -10,24 +10,26 @@ const displayController = (() =>{
         let turn = document.querySelector(".turn");
         if(Gameboard.gameBoard[id-1] === ""){
             if(player1.theirTurn){
-                turn.innerText = "Player O's turn"
+                turn.innerText = `${player2.name}'s turn - ${player2.symbol}`
                 Gameboard.gameBoard[id-1] = player1.symbol;
                 render(Gameboard.gameBoard);
-                checkWin(player1.symbol);
+                checkWin(player1.symbol, player1.name);
                 player1.theirTurn = false;
                 player2.theirTurn = true;
             } else if (player2.theirTurn){
-                turn.innerText = "Player X's turn"
+                turn.innerText = `${player1.name}'s turn - ${player1.symbol}`
                 Gameboard.gameBoard[id-1] = player2.symbol;
                 render(Gameboard.gameBoard);
-                checkWin(player2.symbol);
+                checkWin(player2.symbol, player2.name);
                 player1.theirTurn = true;
                 player2.theirTurn = false;
             }
         }
     }
 
-    const checkWin = (symbol) => {
+    //check if a player's symbol positions have a combination of a winning array position
+    //create a prompt if there is a winner or a tie and ask for a rematch
+    const checkWin = (symbol, name) => {
 
         let win = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]];
         let newArr = [];
@@ -36,16 +38,15 @@ const displayController = (() =>{
                 newArr.push(i+1);
             }
         }
-    
+        let turn = document.querySelector(".turn");
         let checker = (arr, target) => target.every(v => arr.includes(v));
 
         for (let i = 0; i < win.length; i++){
             if (checker(newArr, win[i])){
-                turn.innerText = `${symbol} wins!` ;
+                turn.innerText = `${name} wins!` ;
                 break;
             }
             else if (!Gameboard.gameBoard.includes("") && i === win.length - 1){
-                let turn = document.querySelector(".turn");
                 turn.innerText = "It's a tie!"
                 break;
             }
@@ -66,62 +67,6 @@ const PlayerFactory = (name, symbol, theirTurn) =>{
     return {name, symbol, theirTurn}
 }
 
-
-
-function restart(){
-
-}
-
-function start(){
-    if (validation()){
-        let player1 = document.getElementById("player1").value
-        let player2 = document.getElementById("player2").value
-        GameStart.createPlayer(player1);
-        GameStart.createPlayer(player2);
-        clear();
-        createBoard();
-    }
-}
-
-
-function clear(){
-    const main = document.querySelector("main");
-    main.textContent = "";
-}
-
-function validation(){
-    let player1 = document.getElementById("player1").value
-    let player2 = document.getElementById("player2").value
-    if (player1 === "" || player2 === ""){
-        alert("Please enter a name of each player");
-        return false;
-    }
-    return true;
-}
-
-
-function createBoard(){
-    const main = document.querySelector("main");
-    const numberOfSquare = 9;
-    let grid = document.createElement("div");
-    grid.setAttribute("class", "grid");
-    main.append(grid);
-
-    let body = document.querySelector("body");
-    let turn = document.createElement("h6");
-    turn.setAttribute("class","turn");
-    turn.innerText = "Player X's Turn"
-    body.insertBefore(turn, body.childNodes[2]);
-
-    for(let i = 1; i <= numberOfSquare; i++){
-        let square = document.createElement("div")
-        square.setAttribute("class", "square");
-        square.setAttribute("id", i);
-        square.setAttribute("onclick", `displayController.addMark(${i})`)
-        grid.append(square);
-    }
-}
-
 const GameStart = (() =>{
 
     let playerCount = 0;
@@ -136,6 +81,73 @@ const GameStart = (() =>{
         } 
     }
 
-    return {createPlayer}
+    //create player and reset the board.
+    const start = () =>{
+        if (validation()){
+            let player1 = document.getElementById("player1").value
+            let player2 = document.getElementById("player2").value
+            createPlayer(player1);
+            createPlayer(player2);
+            clear();
+            createBoard();
+        }
+    }
+    
+    //clear the child of main element and reset the gameboard
+    const clear = () =>{
+        const main = document.querySelector("main");
+        main.textContent = "";
+        Gameboard.gameBoard = ['', '', '', '', '', '', '', '', ''];
+    }
+    
+    const validation = () => {
+        let player1 = document.getElementById("player1").value
+        let player2 = document.getElementById("player2").value
+        if (player1 === "" || player2 === ""){
+            alert("Please enter a name of each player");
+            return false;
+        }
+        return true;
+    }
+    
+    
+    const createBoard = () =>{
+        const main = document.querySelector("main");
+        const numberOfSquare = 9;
+        let grid = document.createElement("div");
+        grid.setAttribute("class", "grid");
+        main.append(grid);
+    
+        let body = document.querySelector("body");
+        let turn = document.createElement("h6");
+        turn.setAttribute("class","turn");
+        turn.innerText = `${player1.name}'s turn  - ${player1.symbol}`;
+        body.insertBefore(turn, body.childNodes[2]);
+        
+        let restart = document.createElement("button");
+        restart.innerText = "restart";
+        restart.setAttribute("class","restart");
+        restart.setAttribute("class","btn-primary");
+        restart.setAttribute("onclick","GameStart.restart()");
+        main.appendChild(restart);
+
+        for(let i = 1; i <= numberOfSquare; i++){
+            let square = document.createElement("div")
+            square.setAttribute("class", "square");
+            square.setAttribute("id", i);
+            square.setAttribute("onclick", `displayController.addMark(${i})`)
+            grid.append(square);
+        }
+    }
+
+    function restart(){
+        let turn = document.querySelector(".turn");
+        turn.remove();
+        player1.theirTurn = true;
+        player2.theirTurn = false;
+        clear();
+        createBoard();
+    }
+    return {createPlayer, start, restart}
 })();
 
